4e81016a3a975a7a4eee433469e01edd44c89a27
Felicia-Lindblad <lindblad.felicia@gmail.com>
2024-09-26 20:16:34 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _1formerProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square = new Square();
+
+            Console.WriteLine("Write the lenght of the squares side in cm:");
+            square.side = Convert.ToDouble(Console.ReadLine());
+
+            double squareArea = square.CalculateArea();
+            Console.WriteLine($"Area of the square is {squareArea} cm.");
+
+            Rectangle rectangle = new Rectangle();
+
+            Console.WriteLine("Write the lenght of the rectangle in cm here:");
+            rectangle.side1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Write the height of the rectangle here:");
+            rectangle.side2 = Convert.ToDouble(Console.ReadLine());
+
+            double rectangleArea = rectangle.CalculateArea();
+            Console.WriteLine($"Area of the rectangle is {rectangleArea} cm.");
+
+            Cercle cercle = new Cercle();
+
+            Console.WriteLine("Write the lenght of the cercles radie in cm:");
+            cercle.radius = Convert.ToDouble(Console.ReadLine());
+
+            double cercleArea = cercle.CalculateArea();
+            Console.WriteLine($"Area of the cercle is {cercleArea} cm.");
+        }
+    }
+}
---
+++ Shape.cs
@@ -0,0 +1,36 @@
+﻿namespace _1formerProject
+{
+    public abstract class Shape
+    {
+        public abstract double CalculateArea();
+    }
+
+    public class Square : Shape
+    {
+        public double side;
+        public override double CalculateArea()
+        {
+            return side*side;
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public double side1;
+        public double side2;
+        public override double CalculateArea()
+        {
+            return side1 * side2;
+        }
+    }
+
+    public class Cercle : Shape
+    {
+        public double radius;
+        public override double CalculateArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+
+}
